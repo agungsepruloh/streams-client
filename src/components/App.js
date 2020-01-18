81c5c62e52e9ffbdbb8397c169f1fc54102f8521
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-18 16:10:54 +0700

    Wire Up Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      Page One <Link to="/pagetwo">Go to Page Two</Link>
-      {/* BAD !!! */}
-      {/* <a href="/pagetwo">Go to page two</a> */}
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two <Link to="/">Go to Page One</Link>
-      {/* BAD !!! */}
-      {/* <a href="/">Go to page one</a> */}
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" exact component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
