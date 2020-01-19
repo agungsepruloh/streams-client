b90c5f47f1e285fd32a2b98ba53414ad72267dcd
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-20 05:58:51 +0700

    Update Auth State

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -12,11 +12,19 @@ class GoogleAuth extends React.Component {
           scope: "email"
         })
         .then(() => {
+          // auth instance
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+          // listen auth status
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn) {
