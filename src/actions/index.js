b1232d302c10d9b1dbd0662d29f12789fdb4bf3b
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-22 11:08:38 +0700

    Bulk Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 import streams from "../apis/streams";
 
 export const signIn = userId => {
@@ -26,3 +34,27 @@ export const createStream = formValues => async dispatch => {
 //     streams.post("/streams", formValues);
 //   };
 // };
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = id => async dispatch => {
+  const response = await streams.put(`/streams/${id}`);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
