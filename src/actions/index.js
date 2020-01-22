697527b7bf7e59cede3d7b8e08f15dd89a3b180e
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-22 10:43:45 +0700

    Fixed Typo

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,7 +16,7 @@ export const signOut = () => {
 
 // Shorted syntax
 export const createStream = formValues => async dispatch => {
-  const response = await treams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
