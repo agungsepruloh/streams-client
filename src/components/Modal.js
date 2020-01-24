63f64c80538a89aa033354c1bbd8836dee5735f2
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-24 16:19:28 +0700

    Hide a Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = () => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Here is My Modal!</div>
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={event => event.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure want to delete this stream?</div>
+        <div className="actions">
+          <button className="ui button primary">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
